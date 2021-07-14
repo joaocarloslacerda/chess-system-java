29161723a99e60a7dcc68e66e3008e47300ac477
Joao Carlos Lacerda <juniorlacerdasilva@gmail.com>
2021-07-14 06:37:54 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,42 @@
+package boardgame;
+
+public class Position {
+	
+	//atributos da classe
+	private int row;
+	private int column;
+	
+	//criando construtor personalizado tendo como parâmetro row e column
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	//retorna o atributo row
+	public int getRow() {
+		return row;
+	}
+	
+	//seta um valor para row
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	//retorna o atributo column
+	public int getColumn() {
+		return column;
+	}
+	
+	//seta um valor para column
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	//printando o valor no atributo row e column
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+
+}
